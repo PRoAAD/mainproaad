7061511e2ec395eb7a9146f86f79c66afabf1d56
Administracion PRoA <149621930+PRoAAD@users.noreply.github.com>
2023-11-13 14:46:54 -0300

    actu2

---
+++ PROA AD/_test/profsele.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+        }
+
+        .title {
+            text-align: center;
+            background-color: gray;
+            color: white;
+            padding: 10px;
+            margin-bottom: 20px;
+        }
+
+        .profesor {
+            text-align: center;
+            padding: 10px;
+            margin-bottom: 20px;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        th, td {
+            border: 1px solid black;
+            padding: 8px;
+            text-align: center;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+
+<h2 class="title">Mi cursos</h2>
+<p class="profesor">Profesor: (Nombre Apellido)</p>
+
+<table>
+    <tr>
+        <th>Aula 1v</th>
+    </tr>
+    <tr>
+        <td>Aula 1v</td>
+    </tr>
+</table>
+
+</body>
+</html>
